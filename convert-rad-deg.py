35111384ddf4133de67fbcc0e73e296875a701c3
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2023-12-25 19:04:36 +0100

    Update convert-rad-deg.py

--- convert-rad-deg.py
+++ convert-rad-deg.py
@@ -1,4 +1,4 @@
-# Conversion de degrés, minutes, secondes en radian
+# 1 Conversion de degrés, minutes, secondes en radian
 
 d, m, s = 30, 15, 45 # valeurs degrés, minutes, secondes
 
@@ -7,3 +7,23 @@ d, m, s = 30, 15, 45 # valeurs degrés, minutes, secondes
 rd = (d + m/60 + s/3600) * π/180 # conversion de degrés, minutes, secondes à radians
 
 print (d,"° ", m,"' ", s,'" = ', rd, " rad")
+
+# 2 conversion de radian à degrés, minutes, seconde
+
+rad = 3.562588 # valeur en radian 
+
+π = 3.14159265359
+
+deg = 180/π * rad #degrés 
+
+deg_rnd = round(deg) 
+
+min = (deg - deg_rnd) * 60 #minutes
+
+min_rnd = round(min)
+
+sec = (min - min_rnd) * 60 #secondes
+
+sec_rnd = round(sec)
+
+print (rad, "rad = ", deg_rnd, "° ", min_rnd, "' ", sec_rnd, '"')
