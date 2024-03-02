98d1e4b955719427fbf2b7aabd32fdbfa3620986
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-02 23:43:10 +0100

    update change_charactère.py

--- change_charactère.py
+++ change_charactère.py
@@ -1,3 +1,10 @@
 # fonction pour changer le caractère dans une chaîne.
-
 def changeCar(ch,ca1,ca2,debut,fin):
+  c = 0
+  while c < len(ch):
+    if ch[c] == '':
+      ch[c] = '*'
+    c += 1
+    
+# Appel fonction
+phrase = 'Ceci est une phrase courte'
