145dfbb61049f7d43b44f04111b6250d427961fe
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-03 00:14:11 +0100

    update change_charactère.py

--- change_charactère.py
+++ change_charactère.py
@@ -3,16 +3,16 @@ def changeCar(ch,ca1,ca2,debut,fin):
   c = 0
   nCh = ''
   while c < len(ch):
-    if ch[c] == ' ':
+    if ch[c] == ca1:
       c += 1
-      nCh = nCh + '*'
+      nCh = nCh + ca2
     nCh = nCh + ch[c]
     c += 1
   return  nCh
   
 # Appel fonction
 phrase = 'Ceci est une phrase courte'
-ca1 = ''
+ca1 = ' '
 ca2 = '*'
 debut = 6
 fin = 12
