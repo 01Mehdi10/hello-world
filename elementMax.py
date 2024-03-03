8ea7925dc5512b114e93d0de3dad8a57c113d013
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-03 15:25:46 +0100

    update elementMax.py

--- elementMax.py
+++ elementMax.py
@@ -3,14 +3,23 @@
 def eleMax(liste,debut,fin):
   c = 0
   e = 0
-  while c < len(liste):
-    if liste[c] > e:
-      e = liste[c]
-    c += 1
+  
+  # Sans arguments
+  if debut == '' and fin == '':
+    while c < len(liste):
+      if liste[c] > e:
+        e = liste[c]
+      c += 1
+  # Avec les deux arguments
+  if debut != '' and fin != '':
+    while debut < liste[fin]:
+      if liste[debut] > e:
+        e = liste[debut]
+      debut += 1
   return e
     
 # appel fonction
 liste = [5,8,4,1,2,7,5,9,3,10,0]
-debut = 3
-fin = 9
+debut = 0
+fin = 5
 print("L'élément le plus grand est ", eleMax(liste,debut,fin))
