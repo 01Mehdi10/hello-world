bbdcc675185c4dfb9d3b1fa7df6d01ec1e5e0a41
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-03 15:02:36 +0100

    update elementMax.py

--- elementMax.py
+++ elementMax.py
@@ -1,3 +1,14 @@
-# Affiche l'élément le plus grand dans une liste
+# Affiche l'élément le plus grand d'une liste
 
-def eleMax():
+def eleMax(liste):
+  c = 0
+  e = 0
+  while c < len(liste):
+    if liste[c] > e:
+      e = liste[c]
+    c += 1
+  return e
+    
+# appel fonction
+liste = [5,8,4,1,2,7,5]
+print("L'élément le plus grand est ", eleMax(liste))
