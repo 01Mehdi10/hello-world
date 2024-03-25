4ac355d120b217d955de64a0e164eaf5da59bc88
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-25 18:00:35 +0100

    update damier.py

--- damier.py
+++ damier.py
@@ -1,5 +1,7 @@
 from tkinter import *
-def rang(y,y1,color):
+from random import randrange
+
+def rang(y,y1,color): # rangées de cases
     c = 0
     x = 10
     x1 = 110
@@ -8,7 +10,7 @@ def rang(y,y1,color):
         x += 100
         x1 += 100
         c += 1
-        
+    
 def damier():
     c = 1
     y = 10
@@ -25,12 +27,32 @@ def damier():
         y1 += 100
         c += 1
         
+def pion():
+    x = 16
+    y = 102
+    l1 = [16]
+    l2 = [102]
+    c = 1
+    while c < 10:
+        x += 100
+        y += 100
+        l1.append(x)
+        l2.append(y)
+        c +=1
+    c1 = randrange(len(l1))
+    x = l2[c1]
+    x1 = l1[c1]
+    y = l1[c1]
+    y1 = l2[c1]
+    can.create_oval(x,x1,y,y1,fill='red')
+        
+        
 ##### Programme principal #####
 fen = Tk()
 can = Canvas(fen, width=1020, height=1020, bg='white')
 can.pack(side=TOP, padx=5, pady=5)
 b1 = Button(fen, text='Damier',command=damier)
 b1.pack(side=LEFT, padx=3, pady=3)
-b2 = Button(fen, text='Pions')
+b2 = Button(fen, text='Pions',command=pion)
 b2.pack(side=RIGHT, padx=3, pady=3)
 fen.mainloop()
