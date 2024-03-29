f0f8c04e28e0ebfcf04b4fee5aebaae6f0460aec
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-03-29 09:23:13 +0100

    update Click_cercle.py

---
+++ Click_cercle.py
@@ -0,0 +1,16 @@
+# Affichage d'un petit cercle sur' clic de souris dans une fenêtre :
+from tkinter import *
+def pointeur(event):
+   r = 15
+   chaine.configure(text = "Clic détecté en X =" + str(event.x) +\
+            ", Y =" + str(event.y))
+   X = event.x
+   Y = event.y
+   cadre.create_oval(X-r,Y-r,X+r,Y+r,fill='red')
+fen = Tk()
+cadre = Canvas(fen, width =800, height =800, bg="light yellow")
+cadre.bind("<Button-1>", pointeur)
+cadre.pack()
+chaine = Label(fen)
+chaine.pack()
+fen.mainloop()
