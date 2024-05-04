9ba6ba6fa5c58b4249dfdcc4ab8605ffa6f67e6f
01Mehdi10 <111457232+01Mehdi10@users.noreply.github.com>
2024-05-04 22:30:11 +0200

    Update coulomb_simu.py

--- coulomb_simu.py
+++ coulomb_simu.py
@@ -11,13 +11,13 @@ def avance_c1(event):
  x1, y1 = event.x, event.y
  can1.coords(oval1, x1-15,y1-15, x1+15,y1+15)
  distance_c1_c2()
- 
+
 def avance_c2(event):
  global x2, y2
  x2, y2 = event.x, event.y
  can1.coords(oval2, x2-15,y2-15, x2+15,y2+15)
  distance_c1_c2()
- 
+
 def change_signe_plus():
     global s
     if s == -1:
@@ -27,27 +27,27 @@ def change_signe_moins():
     global s
     if s == 1:
         s = -1
- 
+
 def distance_c1_c2():
  global ech
  D_c1_c2 = round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5,2) # distance charge1 -> charge2
- 
+
  Fe_c1_c2 = G * ((s*vl_charge1) * (s*vl_charge2))/D_c1_c2
  Fe_c1_c2 = round(Fe_c1_c2,2) # force éléctrostatique charge1 -> charge2 
 
  if D_c1_c2 < (15/4 + 15/4):  # Si les charges se chevauchent
-   e_charges.config(text='Échelle de distance : \n' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance charge 1 -> charge 2 : 0 mm \n Force électrostatique charge 1 -> charge 2 : \n' + str(Fe_c1_c2) + ' N'+ '\n Signe = '+str(s)) # échelle de distance, distance entre les charges, force éléctrostatique
+   e_charges.config(text='Échelle : ' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance : 0 mm \n Force : ' + str(Fe_c1_c2) + ' N') # échelle de distance, distance entre les charges, force éléctrostatique
  else:
    D_c1_c2 = round(D_c1_c2 * ech, 2)
-   e_charges.config(text='Échelle de distance : \n' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance charge 1 -> charge 2 : ' + str(D_c1_c2) + ' mm \n Force électrostatique charge 1 -> charge 2 : \n' + str(Fe_c1_c2) + ' N'+ '\n Signe = '+str(s)) # échelle de distance, distance entre les charges, force éléctrostatique
-    
+   e_charges.config(text='Échelle :' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance : ' + str(D_c1_c2) + ' mm \n Force : ' + str(Fe_c1_c2) + ' N') # échelle de distance, distance entre les charges, force éléctrostatique
+
 #------ Programme principal -------
 # les variables suivantes seront utilisées de manière globale :
 
 ech = 0.01 # calcul échelle de distance
 
-x1, y1 = 150, 150  # coordonnées initiales charge 1
-x2, y2 = 170,160 # coordonnées initiales charge 2
+x1, y1 = 200, 200  # coordonnées initiales charge 1
+x2, y2 = 220,220 # coordonnées initiales charge 2
 
 # Calcul distance charge1 -> charge2
 D_c1_c2 = round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5,2)
@@ -68,15 +68,14 @@ fen1.title("Force électrostatique")
 ### création des widgets "esclaves" :
 ###    
 
-can1 = Canvas(fen1,bg='dark grey',height=300,width=300) 
+can1 = Canvas(fen1,bg='dark grey',height=400,width=400) 
 can1.grid(column=0,row=1)# Canvas 
 
 oval1 = can1.create_oval(x1-15,y1-15,x1+15,y1+15,width=2,fill='yellow') # charge 1
 
 oval2 = can1.create_oval(x2-15,y2-15,x2+15,y2+15,width=2,fill='blue') # charge 2
 
-e_charges = Label(fen1,text='Échelle de distance : \n' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance charge 1 -> charge 2 : ' + str(D_c1_c2) +
-                 ' mm \n Force éléctrostatique charge 1 -> charge 2 : \n' + str(Fe_c1_c2) + ' N' + '\n Signe = '+str(s)) # Label du bas qui affiche l'échelle de distance, distance entre les astres, forces gravitationnelles
+e_charges = Label(fen1,text='Échelle : ' + '1 px = ' + str(round(ech, 2)) + ' mm' + '\n Distance : ' + str(D_c1_c2) + ' mm \n Force : ' + str(Fe_c1_c2) + ' N' ) # Label du bas qui affiche l'échelle de distance, distance entre les astres, forces gravitationnelles
 e_charges.grid(column=0,row=3)
 
 frame = Frame(fen1)
